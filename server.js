278d9ee0bc9382a363c95f77fb8bb0f6a57680ed
Jondo <chet9880@gmail.com>
2022-07-26 00:02:13 -0400

    random user id gen

--- server.js
+++ server.js
@@ -2,20 +2,45 @@ const fs = require('fs')
 const express = require('express')
 const path = require("path")
 
+const db = require('./db/db.json')
 /* const home = require('./public/index.html')
 const notes = require('./public/notes.html') */
 const PORT = process.env.PORT || 5000;
 const app = express()
 
 app.use(express.static('public'))
-
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+//main domain
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'))
 })
-
+//notes page
 app.get("/notes", (req, res) =>
 {
   res.sendFile(path.join(__dirname, '/public/notes.html'))
+})
+
+// api notes get 
+app.get('/api/notes', (req, res) =>{
+  fs.readFile('./db/db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+    } else{
+      return res.json(db);
+    }
+  })
+})
+// api notes post request
+app.post('/api/notes', (req, res) =>{
+  const newNote = {
+    
+  }
+})
+
+// optional delete note delete request
+app.delete('/api/notes/:id', (req, res) =>{
+
 })
 
 app.listen(PORT, () => {
